4d9f849732eec9ec66058637aa84aea0d75406b7
Ron <ron@fnal.gov>
2020-08-05 13:06:05 -0500

    move things around and use enum class lvl_t

--- test/basic_functionality_example.cxx
+++ test/basic_functionality_example.cxx
@@ -1,4 +1,12 @@
+/*
+  cd build/logging
+  make clean basic_functionality_example CXX_DEFINES=-DTRY_COMPILE=0
+  test/basic_functionality_example
+ */
+
 #include <logging/logging.hpp>
+
+#define TRACE_NAME "basic_functionality_example" // next version (after v3_15_09) will do this automatically
 
 /** \def ers::File This is the base class for all file related issues.
 */
@@ -18,26 +26,61 @@ ERS_DECLARE_ISSUE_BASE_HPP(ers,	// namespace
                            ERS_EMPTY				   // no attributes in this class
                            )
 
+void ex_thread( int *spinlock )
+{
+	while(*spinlock);
+	for (unsigned uu=0; uu<5; ++uu)
+		LOG_DEBUG(d08) << "fast "<<uu;
+}
+
 
 int main(int argc, char *argv[])
 {
 	std::vector<std::string> arguments(argv + 1, argv + argc);
+
+	// activate TRACE memory buffer for debugging
 	setenv("TRACE_MSGMAX","0",0);
-	setenv("TRACE_LVLS","-1",0);
 	setenv("TRACE_LVLM","-1",0);
+	setenv("TRACE_LVLS","0xff",0);
+	TRACE_CNTL("reset");
+
 	Logger().setup(arguments);
-	LOG_FATAL() << ers::CantOpenFile( ERS_HERE, "MyFatalFileName" );
-	LOG_ERROR() << ers::CantOpenFile( ERS_HERE, "MyErrorFileName" );
-	LOG_WARNING() << ers::CantOpenFile( ERS_HERE, "MyWarnFileName" );
+	LOG_INFO() << "The Logger has just been setup and this is the first Info log message (LOG_INFO).";
+
+//ers::error( ers::Message( ERS_HERE, "this is ers::error( ers::Message( ERS_HERE, \"this is ...\" ) )" ) );
+
+	LOG_FATAL() << ers::CantOpenFile(ERS_HERE,"My_Fatal_FileName");
+	LOG_ERROR() << ers::CantOpenFile(ERS_HERE,"My_Error_FileName");
 #   if TRY_COMPILE & 0x1
-	// see make happeins with: make clean install CXX_DEFINES=-DTRY_COMPILE=1
-	LOG_ERROR() << "just a string";
+	// see what happens with: make clean install CXX_DEFINES=-DTRY_COMPILE=1
+	LOG_ERROR() << "error with just a string";
 #   endif
-	//LOG_INFO()
-	//LOG_LOG()
-	LOG_DEBUG(0) << ers::CantOpenFile( ERS_HERE, "MyDebugFileName" );
-	LOG_DEBUG(0) << "hello - debug level 0";
-	LOG_DEBUG(1) << "hello - debug level 1";
-	system( "trace_cntl show -c10 | trace_delta" );
+	LOG_WARNING() << ers::CantOpenFile(ERS_HERE,"My_Warn_FileName");
+	LOG_WARNING(ignore) << ers::Message(ERS_HERE,"My_Warn_Message with ignored macro param");
+
+
+	LOG_INFO() << "info uses ers::Message";
+#   if TRY_COMPILE & 0x2
+	// see what happens with: make clean install CXX_DEFINES=-DTRY_COMPILE=2
+	LOG_INFO() << ers::Message(ERS_HERE,"a specific TraceStreamer method with ers::Message isn't defined.");
+#   endif
+	LOG_LOG()  << "LOG_LOG also uses ers::Message";
+	LOG_LOG("TEST1")  << ers::CantOpenFile(ERS_HERE,"My_Log_FileName");
+
+	LOG_DEBUG(d00) << "hello - debug level 0 mapped to 5";
+	LOG_DEBUG(d05) << "hello - debug level 5";
+	LOG_DEBUG(d06) << "hello - debug level 6";
+	LOG_DEBUG(d07) << ers::CantOpenFile(ERS_HERE,"My_d07_FileName");
+
+	LOG_DEBUG(d08,"TEST2") << "testing name argument";
+
+	std::cout << "\ntshow follows:\n\n";
+	system( "TRACE_SHOW=\"%H%x%N %T %P %i %C %n %l %R %m\" trace_cntl show | trace_delta -ct 1 -d 1" );
+
+	std::cout << "\nOne could try the same with TDAQ_ERS_VERBOSITY_LEVEL=2 or 3\n";
+
+
+
+
 	return (0);
 }   // main
