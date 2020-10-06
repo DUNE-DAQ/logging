e03c93467f4fcc43ca8e83f8586577ac55cd708e
Ron <ron@fnal.gov>
2020-10-06 16:30:07 -0500

    add debug_speed executable adjust TDAQ_ERS_DEBUG_LEVEL processing -- TRACE_LVLS add TRACE_NAME to six_streams

---
+++ test/debug_speed.cxx
@@ -0,0 +1,49 @@
+/**
+ * @file debug_speed.cxx - demonstrate debug message speed when Issues are used.
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ *
+ */
+
+#include <logging/Logger.hpp>
+
+ERS_DECLARE_ISSUE(ers,		// namespace
+                  File2,		// issue class name
+                  "simple message with file="<<file_name, // no message
+                  ((const char *)file_name ) // single attribute
+                  )
+
+
+int main(int argc, char *argv[])
+{
+	// activate TRACE memory buffer for debugging
+	std::string tfile="/tmp/trace_buffer_"+std::string(getenv("USER"))+"_debug_speed";
+	system( ("rm -f "+tfile).c_str() );
+	setenv("TRACE_FILE",tfile.c_str(),0);
+
+	static const int dbglvl=1;
+	setenv("TRACE_LVLM",std::to_string(1ULL<<(TLVL_DEBUG+dbglvl)).c_str(),0); // make sure the specific debug lvl used in enabled
+
+	if (argc > 1) {
+		std::vector<std::string> arguments(argv + 1, argv + argc);
+		Logger().setup(arguments);	// either do this or export TDAQ_ERS_FATAL=erstrace,lstderr TDAQ_ERS_ERROR='erstrace,throttle(30,100),lstderr' TDAQ_ERS_WARNING='erstrace,throttle(30,100),lstderr'
+	}
+
+	ers::File2 efile2(ERS_HERE, "constructed file" );
+
+	for (int ii=0; ii<10; ++ii)
+		LOG_DEBUG(dbglvl) << ers::File2( ERS_HERE, "construct file" );
+
+	for (int ii=0; ii<10; ++ii)
+		LOG_DEBUG(dbglvl) << efile2;
+
+	for (int ii=0; ii<10; ++ii)
+		LOG_DEBUG(dbglvl) << "message file does not exist";
+	
+	LOG_INFO() << "\ntshow follows:\n\n";
+	system( "trace_cntl show | trace_delta -ct 1 -d 1" );
+
+	return (0);
+}   // main
