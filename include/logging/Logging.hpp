ba55c3738ebbf132a486d37e69db255620cd988c
eflumerf <61473357+eflumerf@users.noreply.github.com>
2022-12-12 15:40:42 -0600

    Revert "Eric's clang format fixes"

--- include/logging/Logging.hpp
+++ include/logging/Logging.hpp
@@ -8,11 +8,11 @@
 #ifndef LOGGING_INCLUDE_LOGGING_LOGGING_HPP_
 #define LOGGING_INCLUDE_LOGGING_LOGGING_HPP_
 
-#include "TRACE/trace.h"
-#include "ers/OutputStream.hpp"
-#include "ers/ers.hpp"
 #include <string>
 #include <vector>
+#include "ers/ers.hpp"
+#include "ers/OutputStream.hpp"
+#include "TRACE/trace.h"
 
 #include "logging/internal/macro.hpp"
 
@@ -24,72 +24,67 @@ namespace dunedaq::logging {
 class Logging
 {
 public:
+
   /**
    * @brief Setup the Logger service
    * Currently no args.
    */
-  static void setup(void)
-  {
-    // need to get tricky to short circuit DEBUG message (at "level 1") about
-    //    libmtsStreams.so: cannot open shared object file: No such file or directory
-    /** Example: production env:
-            export DUNEDAQ_ERS_ERROR="erstrace,throttle(30,100),lstderr,mts"
-            export DUNEDAQ_ERS_FATAL="erstrace,lstderr,mts"
-            export DUNEDAQ_ERS_WARNING="erstrace,throttle(30,100),lstderr,mts"
-            export DUNEDAQ_ERS_INFO="erstrace,throttle(30,100),lstdout,mts"
+	static void setup( void )
+	{
+		// need to get tricky to short circuit DEBUG message (at "level 1") about
+		//    libmtsStreams.so: cannot open shared object file: No such file or directory		
+		/** Example: production env:
+			export DUNEDAQ_ERS_ERROR="erstrace,throttle(30,100),lstderr,mts"
+			export DUNEDAQ_ERS_FATAL="erstrace,lstderr,mts"
+			export DUNEDAQ_ERS_WARNING="erstrace,throttle(30,100),lstderr,mts"
+			export DUNEDAQ_ERS_INFO="erstrace,throttle(30,100),lstdout,mts"
 
-            export DUNEDAQ_ERS_LOG="lstdout"
-            export DUNEDAQ_ERS_DEBUG="lstdout"
+			export DUNEDAQ_ERS_LOG="lstdout"
+			export DUNEDAQ_ERS_DEBUG="lstdout"
 
-            export DUNEDAQ_ERS_STREAM_LIBS="mtsStreams"
-     */
-    // set defaults with 0=no override
-    setenv("DUNEDAQ_ERS_FATAL", "erstrace,lstderr", 0);
-    setenv("DUNEDAQ_ERS_ERROR", "erstrace,throttle(30,100),lstderr", 0);
-    setenv("DUNEDAQ_ERS_WARNING", "erstrace,throttle(30,100),lstderr", 0);
-    setenv("DUNEDAQ_ERS_INFO", "erstrace,lstdout", 0);
-    setenv("DUNEDAQ_ERS_LOG", "lstdout", 0);
-    setenv("DUNEDAQ_ERS_DEBUG", "lstdout", 0);
-    std::vector<std::string> envvars = {
-      "DUNEDAQ_ERS_FATAL", "DUNEDAQ_ERS_ERROR", "DUNEDAQ_ERS_WARNING", "DUNEDAQ_ERS_INFO"
-    };
-    for (std::string& envvar : envvars) {
-      char* ecp = getenv(envvar.c_str());
-      if (strncmp(ecp, "erstrace", 8) != 0) {
-        std::string newval = "erstrace," + std::string(ecp);
-        setenv(envvar.c_str(), newval.c_str(), 1);
-      }
-    }
+			export DUNEDAQ_ERS_STREAM_LIBS="mtsStreams"
+		 */
+		// set defaults with 0=no override
+		setenv("DUNEDAQ_ERS_FATAL", "erstrace,lstderr",0);
+		setenv("DUNEDAQ_ERS_ERROR", "erstrace,throttle(30,100),lstderr",0);
+		setenv("DUNEDAQ_ERS_WARNING","erstrace,throttle(30,100),lstderr",0);
+		setenv("DUNEDAQ_ERS_INFO",   "erstrace,lstdout",0);
+		setenv("DUNEDAQ_ERS_LOG",    "lstdout",0);
+		setenv("DUNEDAQ_ERS_DEBUG",  "lstdout",0);
+		std::vector<std::string> envvars = {"DUNEDAQ_ERS_FATAL","DUNEDAQ_ERS_ERROR","DUNEDAQ_ERS_WARNING","DUNEDAQ_ERS_INFO"};
+		for (std::string& envvar : envvars) {
+			char *ecp = getenv(envvar.c_str());
+			if (strncmp(ecp,"erstrace",8) != 0) {
+				std::string newval = "erstrace," + std::string(ecp);
+				setenv(envvar.c_str(),newval.c_str(),1);
+			}
+		}
 
-    // setenv("DUNEDAQ_APPLICATION_NAME","XYZZY",0);
-    //  std::ostringstream out;
-    //  out << (1ULL<<(TLVL_DEBUG+2))-1;
-    //  setenv("TRACE_LVLM",(out.str()+",0").c_str(),0);
+		//setenv("DUNEDAQ_APPLICATION_NAME","XYZZY",0);
+		// std::ostringstream out;
+		// out << (1ULL<<(TLVL_DEBUG+2))-1;
+		// setenv("TRACE_LVLM",(out.str()+",0").c_str(),0);
 
-    // Avoid ERS_INTERNAL_DEBUG( 1, "Library " << MRSStreamLibraryName << " can not be loaded because " << ex.reason() )
-    //  DEBUG_1 [ers::PluginManager::PluginManager(...) at ...Library mtsStreams can not be loaded because
-    //  libmtsStreams.so: cannot open shared object file: No such file or directory
-    // by only setting debug_level AFTER first ers::debug message
-    // setenv("DUNEDAQ_ERS_DEBUG_LEVEL","63",0);
-    ers::LocalContext lc("logging package", __FILE__, __LINE__, __func__, 0 /*no_stack*/);
-    int lvl = 100;
-    ers::InternalMessage msg(lc,
-                             "Logger setup(...) ers::debug level " + std::to_string(lvl) +
-                               " -- seems to come out level 0 (with ERS version v0_26_00d) ???");
-    msg.set_severity(ers::Severity(ers::Debug, lvl));
-    ers::debug(msg, lvl); // still comes out as level 0 ???
-    ers::Configuration::instance().debug_level(63);
-    char* cp;
-    if ((cp = getenv("DUNEDAQ_ERS_DEBUG_LEVEL")) && *cp) {
-      int lvl = strtoul(cp, nullptr, 0) + TLVL_DEBUG;
-      if (lvl > 63)
-        lvl = 63;
-      // TRACE_CNTL("lvlmskSg",(1ULL<<lvl)-1); // this sets traceTID to id of "Logger"
-      uint64_t msk = ((1ULL << lvl) - 1) | (1ULL << lvl);
-      std::string mskstr = std::to_string(msk);
-      setenv("TRACE_LVLS", mskstr.c_str(), 0);
-    }
-  }
+		// Avoid ERS_INTERNAL_DEBUG( 1, "Library " << MRSStreamLibraryName << " can not be loaded because " << ex.reason() )
+		//  DEBUG_1 [ers::PluginManager::PluginManager(...) at ...Library mtsStreams can not be loaded because libmtsStreams.so: cannot open shared object file: No such file or directory
+		// by only setting debug_level AFTER first ers::debug message
+		//setenv("DUNEDAQ_ERS_DEBUG_LEVEL","63",0);
+		ers::LocalContext lc( "logging package", __FILE__, __LINE__, __func__, 0/*no_stack*/ );
+		int lvl=100;
+		ers::InternalMessage msg(lc,"Logger setup(...) ers::debug level "+std::to_string(lvl)+" -- seems to come out level 0 (with ERS version v0_26_00d) ???");
+		msg.set_severity( ers::Severity( ers::Debug, lvl ) );
+		ers::debug(msg,lvl); // still comes out as level 0 ???
+		ers::Configuration::instance().debug_level(63);
+		char *cp;
+		if ((cp=getenv("DUNEDAQ_ERS_DEBUG_LEVEL")) && *cp) {
+			int lvl=strtoul(cp,nullptr,0)+TLVL_DEBUG;
+			if (lvl>63) lvl=63;
+			//TRACE_CNTL("lvlmskSg",(1ULL<<lvl)-1); // this sets traceTID to id of "Logger"
+			uint64_t msk = ((1ULL<<lvl)-1) | (1ULL<<lvl);
+			std::string mskstr=std::to_string(msk);
+			setenv("TRACE_LVLS",mskstr.c_str(),0);
+		}
+	}
 };
 
 } // namespace logging
@@ -99,21 +94,19 @@ public:
 //  The following uses gnu extension of "##" connecting "," with empty __VA_ARGS__
 //  which eats "," when __VA_ARGS__ is empty.
 //      pragma GCC diagnostic ignored "-Wvariadic-macros" // doesn't seem to work.
-//  Neither does pragma GCC diagnostic ignored "-Wpedantic" // g++ -std=c++11 --help=warnings | egrep -i
-//  'variadic|pedantic' These are last because "pragma GCC system_header" only gets undone at the end of the file. This
-//  issue is fixed in -std=c++2a
-#if (__cplusplus < 201709L)
-#pragma GCC system_header
-#endif
+//  Neither does pragma GCC diagnostic ignored "-Wpedantic" // g++ -std=c++11 --help=warnings | egrep -i 'variadic|pedantic'
+//  These are last because "pragma GCC system_header" only gets undone at the end of the file.
+//  This issue is fixed in -std=c++2a
+#       if (__cplusplus < 201709L)
+#               pragma GCC system_header
+#       endif
 
 #if TRACE_REVNUM <= 1443
-#undef TLOG_DEBUG
-#define TLOG_DEBUG(lvl, ...)                                                                                           \
-  TRACE_STREAMER(((TLVL_DEBUG + lvl) < 64) ? TLVL_DEBUG + lvl : 63,                                                    \
-                 tlog_ARG2(not_used, ##__VA_ARGS__, 0, need_at_least_one),                                             \
-                 tlog_ARG3(not_used, ##__VA_ARGS__, 0, "", need_at_least_one),                                         \
-                 1,                                                                                                    \
-                 0)
+# undef  TLOG_DEBUG
+# define TLOG_DEBUG(lvl,...) TRACE_STREAMER(((TLVL_DEBUG+lvl)<64)?TLVL_DEBUG+lvl:63, \
+										  tlog_ARG2(not_used, ##__VA_ARGS__,0,need_at_least_one), \
+										  tlog_ARG3(not_used, ##__VA_ARGS__,0,"",need_at_least_one), \
+										  1, 0 )
 #endif
 
 #endif // LOGGING_INCLUDE_LOGGING_LOGGING_HPP_
--- test/apps/debug_speed.cxx
+++ test/apps/debug_speed.cxx
@@ -19,44 +19,40 @@ Run:
 #include <logging/Logging.hpp>
 #include <string>
 
-ERS_DECLARE_ISSUE(ers,                                      // namespace
-                  File2,                                    // issue class name
-                  "simple message with file=" << file_name, // no message
-                  ((const char*)file_name)                  // single attribute
-)
+ERS_DECLARE_ISSUE(ers,		// namespace
+                  File2,		// issue class name
+                  "simple message with file="<<file_name, // no message
+                  ((const char *)file_name ) // single attribute
+                  )
 
-int
-main(int argc, char* argv[] __attribute__((__unused__)))
+
+int main(int argc, char *argv[] __attribute__((__unused__)))
 {
-  // activate TRACE memory buffer for debugging
-  std::string tfile = "/tmp/trace_buffer_" + std::string(getenv("USER")) + "_debug_speed";
-  system(("rm -f " + tfile).c_str());
-  setenv("TRACE_FILE", tfile.c_str(), 0);
+	// activate TRACE memory buffer for debugging
+	std::string tfile="/tmp/trace_buffer_"+std::string(getenv("USER"))+"_debug_speed";
+	system( ("rm -f "+tfile).c_str() );
+	setenv("TRACE_FILE",tfile.c_str(),0);
 
-  static const int dbglvl = 1;
-  setenv("TRACE_LVLM",
-         std::to_string(1ULL << (TLVL_DEBUG + dbglvl)).c_str(),
-         0); // make sure the specific debug lvl used is enabled
+	static const int dbglvl=1;
+	setenv("TRACE_LVLM",std::to_string(1ULL<<(TLVL_DEBUG+dbglvl)).c_str(),0); // make sure the specific debug lvl used is enabled
 
-  if (argc > 1) {
-    dunedaq::logging::Logging::setup(); // either do this or export DUNEDAQ_ERS_FATAL=erstrace,lstderr
-                                        // DUNEDAQ_ERS_ERROR='erstrace,throttle(30,100),lstderr'
-                                        // DUNEDAQ_ERS_WARNING='erstrace,throttle(30,100),lstderr'
-  }
+	if (argc > 1) {
+		dunedaq::logging::Logging::setup();	// either do this or export DUNEDAQ_ERS_FATAL=erstrace,lstderr DUNEDAQ_ERS_ERROR='erstrace,throttle(30,100),lstderr' DUNEDAQ_ERS_WARNING='erstrace,throttle(30,100),lstderr'
+	}
 
-  ers::File2 efile2(ERS_HERE, "constructed file");
+	ers::File2 efile2(ERS_HERE, "constructed file" );
 
-  for (int ii = 0; ii < 10; ++ii)
-    TLOG_DEBUG(dbglvl) << ers::File2(ERS_HERE, "construct file");
+	for (int ii=0; ii<10; ++ii)
+		TLOG_DEBUG(dbglvl) << ers::File2( ERS_HERE, "construct file" );
 
-  for (int ii = 0; ii < 10; ++ii)
-    TLOG_DEBUG(dbglvl) << efile2;
+	for (int ii=0; ii<10; ++ii)
+		TLOG_DEBUG(dbglvl) << efile2;
 
-  for (int ii = 0; ii < 10; ++ii)
-    TLOG_DEBUG(dbglvl) << "message file does not exist";
+	for (int ii=0; ii<10; ++ii)
+		TLOG_DEBUG(dbglvl) << "message file does not exist";
+	
+	TLOG() << "\ntshow follows:\n\n";
+	system( "TRACE_SHOW=\"%H%x%N %T %P %i %C %e %L %R %m\" trace_cntl show | trace_delta -ct 1 -d 1" );
 
-  TLOG() << "\ntshow follows:\n\n";
-  system("TRACE_SHOW=\"%H%x%N %T %P %i %C %e %L %R %m\" trace_cntl show | trace_delta -ct 1 -d 1");
-
-  return (0);
-} // main
+	return (0);
+}   // main
